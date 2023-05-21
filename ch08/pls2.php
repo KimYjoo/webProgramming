9830bc1f5a2e40b79178b52100c217d7d16636a2
Kim_Yjoo <pas4976@gmail.com>
2023-05-22 02:01:53 +0900

    update pls2

--- ch08/pls2.php
+++ ch08/pls2.php
@@ -7,11 +7,15 @@
 <?php
     session_start();
     if(isset($_POST["check"])){
-        if($_SESSION['count'] >= 5){ 
+        if(ctype_alpha($_POST['user_input'])){ 
+            echo "숫자가 아닙니다. 숫자를 입력해주세요.<br>";
+            
+        }
+        else if($_SESSION['count'] >= 5){
             echo "시도 횟수 : ".$_SESSION['count']."<br>";
             echo "최대 시도 회수가 초과했습니다!";
         }
-        else if(!ctype_alpha($_POST['user_input'])){
+        else {
             $_SESSION['user_input'] = $_POST['user_input'];
             $_SESSION['count']+=1;
             echo "시도 횟수 : ".$_SESSION['count']."<br>";
@@ -19,9 +23,7 @@
             else if($_SESSION['randNum']<$_SESSION['user_input']) echo "당신이 입력한 숫자 ".$_SESSION['user_input']."보다 Down!<br>";
             else echo "당신이 입력한 숫자 ".$_SESSION['user_input']."은 정답입니다!<br>";
             
-            
         }
-        else echo "숫자가 아닙니다. 숫자를 입력해주세요.<br>";
     }
     else{
         $_SESSION['randNum'] = rand(1, 100);
